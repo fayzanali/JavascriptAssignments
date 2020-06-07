480cd839d61dd0989ee1b95122acc0432f8890f1
fayzanali <syedfayzanali7@outlook.com>
2020-06-07 16:28:00 +0500

    commit1

---
+++ app.js
@@ -0,0 +1,90 @@
+// CHAPTER 1
+// Task1:
+alert("Welcome to my Website Global Tagg");
+
+// Task2:
+alert("Error! Please enter a valid password.");
+
+// Task3:
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Task4:
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Task5:
+console.log(alert("Hello... I can JS through my web browser's console"));
+
+// Task6:
+
+// CHAPTER 2
+// Task1:
+var username;
+
+// Task2:
+var myName = "Syed Fayzan Ali";
+
+// Task3:
+var message;
+message = "Hello World";
+alert(message)
+
+// Task4:
+var name = "Syed Fayzan Ali";
+var age = 24;
+var course = "Certified Python Programming";
+
+alert(name);
+alert(age + " years old");
+alert(course);
+
+// Task5:
+var pz = ("PIZZA\nPIZZ\nPIZ\nPI\nP");
+alert(pz);
+
+// Task6:
+var email = "syedfayzanali7@outlook.com";
+alert("My email address is " + email);
+
+// Task7:
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Task8:
+var doc = ("Yah! I can write HTML content through JavaSript");
+document.write(doc);
+
+// Task9:
+var content = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(content);
+
+// CHAPTER 3
+// Task1:
+var age;
+age = 24;
+alert("I am " + age + " years old");
+
+// Task2:
+var visits = ("You have visited this site 14 times");
+alert(visits);
+
+// Task3:
+var birthYear = 1995;
+document.write("<br><br>My birth year is " + birthYear + "<br>" + "Data type of my declared variable is " + typeof(birthYear));
+
+// Task4:
+var visitor_name  = "John Doe";
+var product = "T-shirt";
+var quantity = 5;
+document.write("<br><br>" + visitor_name + " ordered " + quantity + " " + product +"(s) on XYZ Clothing store");
+
+// CHAPTER 4
+// Task1:
+var row = 0, column = 0, index = 0;
+
+// Task2:
+// Legal Variables:
+var name, $name, _name, $age, age;
+// Illegal Variables:
+var 1name, abd+xyz, /abc, abc xyz, @abc;
+
